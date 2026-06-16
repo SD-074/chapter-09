a61d712d53e8785652dfcdc516b0d61d3ddcc25a
Mahmoud Khudairi <mahmoudkhudairi@gmail.com>
2026-06-16 11:14:54 +0300

    add solutions

---
+++ 04)file-upload/backend/src/db.ts
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+try {
+  if (!process.env.MONGO_URI) throw new Error('Missing MONGO_URI');
+  await mongoose.connect(process.env.MONGO_URI, { dbName: 'file-upload' });
+  console.log('MongoDB connected');
+} catch (error) {
+  console.error('MongoDB connection error:', error);
+  process.exit(1);
+}
---
+++ 04)file-upload/backend/src/middlewares/cloudUploader.ts
@@ -0,0 +1,30 @@
+import { v2 as cloudinary } from 'cloudinary';
+import { type RequestHandler } from 'express';
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+  secure_url: true
+});
+
+// Upload an image
+const cloudUploader: RequestHandler = async (req, res, next) => {
+  try {
+    const filePath = req.image!.filepath;
+
+    const cloudinaryData = await cloudinary.uploader.upload(filePath, {
+      resource_type: 'auto'
+    });
+    console.log('WHAT IS THE REQ.body', req.body);
+    console.log('WHat is cloudinary response', cloudinaryData);
+    req.body.image = cloudinaryData.secure_url;
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default cloudUploader;
